0572e496e99317566ba6abbca5e95a019822ed1f
par-paulreyes <124680300+par-paulreyes@users.noreply.github.com>
2024-04-27 11:00:48 +0800

    Add files via upload
    updated the login/register and added mainform

---
+++ Hireko/Forms/MainForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Hireko.Forms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel = new Panel();
+            bntHome = new Button();
+            bntCreateService = new Button();
+            btnProfile = new Button();
+            SuspendLayout();
+            // 
+            // panel
+            // 
+            panel.Location = new Point(36, 67);
+            panel.Name = "panel";
+            panel.Size = new Size(994, 537);
+            panel.TabIndex = 0;
+            panel.Paint += panel_Paint;
+            // 
+            // bntHome
+            // 
+            bntHome.Location = new Point(89, 23);
+            bntHome.Name = "bntHome";
+            bntHome.Size = new Size(94, 29);
+            bntHome.TabIndex = 1;
+            bntHome.Text = "Home";
+            bntHome.UseVisualStyleBackColor = true;
+            bntHome.Click += bntHome_Click;
+            // 
+            // bntCreateService
+            // 
+            bntCreateService.Location = new Point(283, 23);
+            bntCreateService.Name = "bntCreateService";
+            bntCreateService.Size = new Size(138, 29);
+            bntCreateService.TabIndex = 2;
+            bntCreateService.Text = "Sell a Service";
+            bntCreateService.UseVisualStyleBackColor = true;
+            bntCreateService.Click += bntCreateService_Click;
+            // 
+            // btnProfile
+            // 
+            btnProfile.Location = new Point(955, 23);
+            btnProfile.Name = "btnProfile";
+            btnProfile.Size = new Size(94, 29);
+            btnProfile.TabIndex = 3;
+            btnProfile.Text = "Profile";
+            btnProfile.UseVisualStyleBackColor = true;
+            btnProfile.Click += btnProfile_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1079, 629);
+            Controls.Add(btnProfile);
+            Controls.Add(bntCreateService);
+            Controls.Add(bntHome);
+            Controls.Add(panel);
+            Name = "MainForm";
+            Text = "MainForm";
+            Load += MainForm_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel;
+        private Button bntHome;
+        private Button bntCreateService;
+        private Button btnProfile;
+    }
+}
---
+++ Hireko/Forms/RegisterForm.cs
@@ -0,0 +1,147 @@
+﻿using Hireko.Database;
+using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Hireko.Forms
+{
+    public partial class RegisterForm : Form
+    {
+        private string enteredUsername = "";
+        private string enteredPassword = "";
+        private string enteredConfirmPassword = "";
+        private string enteredFirstName = "";
+        private string enteredLastName = "";
+        private string enteredEmail = "";
+        private string enteredPhone = "";
+        private string enteredAddress = "";
+
+        private string connectionString = DatabaseInitializer.GetConnectionString();
+
+        public RegisterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+            enteredUsername = txtUsername.Text;
+        }
+
+        private void txtpassword_TextChanged(object sender, EventArgs e)
+        {
+            enteredPassword = txtPassword.Text;
+        }
+
+        private void txtConfirmPassword_TextChanged(object sender, EventArgs e)
+        {
+            enteredConfirmPassword = txtConfirmPassword.Text;
+        }
+
+        private void txtFirstName_TextChanged(object sender, EventArgs e)
+        {
+            enteredFirstName = txtFirstName.Text;
+        }
+
+        private void txtLastName_TextChanged(object sender, EventArgs e)
+        {
+            enteredLastName = txtLastName.Text;
+        }
+
+        private void txtEmail_TextChanged(object sender, EventArgs e)
+        {
+            enteredEmail = txtEmail.Text;
+        }
+
+        private void txtPhone_TextChanged(object sender, EventArgs e)
+        {
+            enteredPhone = txtPhone.Text;
+        }
+
+        private void txtAddress_TextChanged(object sender, EventArgs e)
+        {
+            enteredAddress = txtAddress.Text;
+        }
+
+        private void bntRegister_Click(object sender, EventArgs e)
+        {
+            if (ValidateForm())
+            {
+                if (enteredPassword != enteredConfirmPassword)
+                {
+                    MessageBox.Show("Passwords do not match. Please re-enter your password.");
+                    return;
+                }
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string query = "INSERT INTO Users (Username, Password, FirstName, LastName, Email, [Address], Phone) VALUES (@Username, @Password, @FirstName, @LastName, @Email, @Address, @Phone)";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@Username", enteredUsername);
+                    command.Parameters.AddWithValue("@Password", enteredPassword);
+                    command.Parameters.AddWithValue("@FirstName", enteredFirstName);
+                    command.Parameters.AddWithValue("@LastName", enteredLastName);
+                    command.Parameters.AddWithValue("@Email", enteredEmail);
+                    command.Parameters.AddWithValue("@Address", enteredAddress);
+                    command.Parameters.AddWithValue("@Phone", enteredPhone);
+
+                    try
+                    {
+                        connection.Open();
+                        int rowsAffected = command.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Registration successful! You can now login with your credentials.");
+                            // Navigate back to the login form
+                            LoginForm loginForm = new LoginForm();
+                            loginForm.Show();
+                            this.Hide();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Registration failed. Please try again.");
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show("Database error: " + ex.Message);
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+        }
+
+        private bool ValidateForm()
+        {
+            if (string.IsNullOrWhiteSpace(enteredUsername) ||
+                string.IsNullOrWhiteSpace(enteredPassword) ||
+                string.IsNullOrWhiteSpace(enteredConfirmPassword) ||
+                string.IsNullOrWhiteSpace(enteredFirstName) ||
+                string.IsNullOrWhiteSpace(enteredLastName) ||
+                string.IsNullOrWhiteSpace(enteredEmail) ||
+                string.IsNullOrWhiteSpace(enteredPhone) ||
+                string.IsNullOrWhiteSpace(enteredAddress))
+            {
+                MessageBox.Show("Please fill in all the required fields.");
+                return false;
+            }
+
+            return true;
+        }
+        private void RegisterForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLogin_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Navigate back to the login form
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+            this.Hide();
+        }
+    }
+}
